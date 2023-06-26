be2cf0540662441509c7a433e0693e7aeff7cefd
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2023-06-26 23:12:12 +0530

    Add description for user

--- models/user.js
+++ models/user.js
@@ -19,6 +19,13 @@ const UserSchema = newSchema({
   },
 });
 
-const User = model("User", UserSchema);
+const User = models.User || model("User", UserSchema);
 
 export default User;
+
+//The "models" object is provided by the mongoose library and stores all the registered models.
+//If a model named "User" already exists in the "models" object, it assigns that existing model to the "User" variable.
+//This prevents redefining the model and ensures that the existing model is reused.
+
+//If a model named "User" does not exist in the "models" object, the "model" function from mongoose is called to create a new model
+//The newly created model is then assigned to the "User" variable.
